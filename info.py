1064c850c4b1dd5fdfc8f2e93387ef48acb8305e
Aniket56829kw <132885070+Aniket56829kw@users.noreply.github.com>
2025-01-10 06:40:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -91,7 +91,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Aniket_Movies_Hub')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Aniket_Movies_Hub')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Aniket_Movies_Hub') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/King18x007)
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/King18x007')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
